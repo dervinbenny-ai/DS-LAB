917d2f3a8568f54e996e31a9055e975aa2bc0888
dervinbenny-ai <dervinbenny@gmail.com>
2025-10-21 14:27:21 +0530

    Add files via upload

---
+++ strct.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+struct employee {
+int emp_id;
+char name[50];
+float salary;
+};
+int main(){
+struct employee e[3];
+int i;
+printf("Enter the details of the employees:\n");
+for(i=0;i<3;i++){
+printf("\employee %d:\n",i+1);
+printf("Enter the employee id:");
+scanf("%d",&e[i].emp_id);
+printf("Enter name:");
+scanf("%s",e[i].name);
+printf("Enter the Salary:");
+scanf("%f",&e[i].salary);
+}
+printf("\n--- employee details ---\n");
+for(i=0;i<3;i++){
+printf("\n employee %d\n",i+1);
+printf("Employee id:%d\n",e[i].emp_id);
+printf("name :%s\n",e[i].name);
+printf("Salary :%.2f\n",e[i].salary);
+}
+return 0;
+}
